7df8bf0299c97a85f3249d6f19bcee7a18e0e361
Josh Freeman <josh@viion.co.uk>
2017-08-11 16:28:14 +0100

    Add journal section and remove if statements cus "lolSE"

--- src/SaintCsvParser/Quest.php
+++ src/SaintCsvParser/Quest.php
@@ -20,6 +20,28 @@ class Quest implements ContentInterface
      */
     private function wiki($quest)
     {
+        // ---------------------------------------------------------------------------
+        // Grab any extra data
+        // ---------------------------------------------------------------------------
+        
+        // grab the raw CSV file based on the same row as the current quest
+        // we only want 1 quest (this quest) so we do `->one()`
+        $raw = $this->getRaw('Quest', $quest->csv_row)->one();
+
+        // grab the genre based on the raw journal_genre id
+        // we only want 1 journal entry so we do `->one()`
+        $genre = $this->get('JournalGenre', $raw->journal_genre)->one();
+
+        // get the raw version of journal genre at the same position as we did above
+        $genreRaw = $this->getRaw('JournalGenre', $raw->journal_genre)->one();
+
+        // grab the category based on the raw position for journal_category
+        $category = $this->get('JournalCategory', $genreRaw->journal_category)->one();
+
+        // ---------------------------------------------------------------------------
+        // Handle output
+        // ---------------------------------------------------------------------------
+
         // wiki format
         $format = '
         {{ARR Infobox Quest
@@ -28,42 +50,30 @@ class Quest implements ContentInterface
         |Level = {level}
         |Genre = {genre}
         |Category = {category}
+        |Section = {section}
         }}';
-        
-        // ---------------------------------------------------------------------------
-        
-        // grab the raw CSV file based on the same row as the current quest
-        // we only want 1 quest (this quest) so we do `->one()`
-        $raw = $this->getRaw('Quest', $quest->csv_row)->one();
-        
-        // default value for genre (some quests don't link to some, placeholders)
-        $genre = null;
-    
-        // if the journal genre id is above 0
-        if ($raw->journal_genre > 0) {
-            // grab the genre based on the raw journal_genre id
-            // we only want 1 journal entry so we do `->one()`
-            $genre = $this->get('JournalGenre', $raw->journal_genre)->one();
-        }
-    
-        // ---------------------------------------------------------------------------
+
 
         // fields
         $data = [
             '{patch}' => $this->app->getPatch(),
             '{name}' => $quest->name,
-            
+
             '{genre}' => $quest->journal_genre,
             '{category}' => $genre ? $genre->journal_category : '',
+            '{section}' => $category ? $category->journal_section : '',
             
             '{level}' => $quest->class_level_0,
         ];
     
         // ---------------------------------------------------------------------------
+        // Clean up and finish
+        // ---------------------------------------------------------------------------
 
         // unset expensive data to free memory
         unset($raw);
         unset($genre);
+        unset($category);
         
         // save
         return $this->format($format, $data);
